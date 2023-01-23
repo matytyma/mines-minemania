a299cef936d69b6bd52da9bfc774a968d2d83b07
Matyáš Vítek <100159609+matytyma@users.noreply.github.com>
2023-01-23 15:04:01 +0100

    Fix ores keeping broken after restart

--- src/main/java/me/kolombooo/minesplugin/Main.java
+++ src/main/java/me/kolombooo/minesplugin/Main.java
@@ -6,11 +6,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.Objects;
 
 public final class Main extends JavaPlugin {
-
 	@Override
 	public void onEnable() {
 		Objects.requireNonNull(getCommand("mines")).setExecutor(new MinesCommand());
 		Bukkit.getPluginManager().registerEvents(new MineListener(), this);
 		Bukkit.getScheduler().runTaskTimer(this, new RespawnChecker(), 0, 20);
+	}
+
+	@Override
+	public void onDisable() {
+		for (BrokenOre ore : BlocksHandler.blocks) {
+			ore.location.getWorld().setBlockData(ore.location, ore.blockData);
+		}
 	}
 }
