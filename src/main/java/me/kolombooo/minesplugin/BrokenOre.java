86985d6408a006a55eb88dd02fb6f7992964f187
Matyáš Vítek <100159609+matytyma@users.noreply.github.com>
2023-01-27 21:32:31 +0100

    Change back from BEDROCK to ANCIENT_DEBRIS

--- src/main/java/me/kolombooo/minesplugin/BrokenOre.java
+++ src/main/java/me/kolombooo/minesplugin/BrokenOre.java
@@ -2,7 +2,6 @@ package me.kolombooo.minesplugin;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
 import org.jetbrains.annotations.NotNull;
 
@@ -12,8 +11,7 @@ public class BrokenOre implements Comparable<BrokenOre> {
 	BlockData blockData;
 
 	public BrokenOre(Material material, Location location, long respawnTime) {
-		System.out.println(material);
-		if (!material.toString().equals("BEDROCK")) {
+		if (!material.toString().equals("ANCIENT_DEBRIS")) {
 			this.blockData = Material.valueOf(material.toString().replace("DEEPSLATE_", "")).createBlockData();
 		} else {
 			this.blockData = Material.ANCIENT_DEBRIS.createBlockData();
