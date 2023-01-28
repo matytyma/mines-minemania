86d473b658130ba881c7bf82a1df79360faa23da
Matyáš Vítek <100159609+matytyma@users.noreply.github.com>
2023-01-28 13:19:51 +0100

    Load configurable messages

--- src/main/java/me/kolombooo/minesplugin/ConfigHandler.java
+++ src/main/java/me/kolombooo/minesplugin/ConfigHandler.java
@@ -1,5 +1,6 @@
 package me.kolombooo.minesplugin;
 
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import java.util.HashMap;
@@ -9,6 +10,16 @@ import java.util.Objects;
 public class ConfigHandler {
 	static Map<String, Integer> dropChances = new HashMap<>();
 	static Map<String, Integer> rewards = new HashMap<>();
+	static String prefix;
+	static String onlyPlayers;
+	static String noPermission;
+	static String pluginInfo;
+	static String successfulReload;
+	static String help;
+	static String helpReload;
+	static String teleportHelp;
+	static String teleportHelpVip;
+	static String teleportHelpAdmin;
 
 	static void reloadConfig() {
 		Main plugin = Main.getPlugin(Main.class);
@@ -16,12 +27,23 @@ public class ConfigHandler {
 		plugin.reloadConfig();
 		FileConfiguration config = plugin.getConfig().options().copyDefaults(true).configuration();
 		dropChances.clear();
-		for (String s : Objects.requireNonNull(config.getConfigurationSection("drop-chance")).getKeys(false)) {
+		for (String s : Objects.requireNonNull(config.getConfigurationSection("drop-chances")).getKeys(false)) {
 			dropChances.put(s, config.getInt("drop-chances." + s));
 		}
 		rewards.clear();
 		for (String s : Objects.requireNonNull(config.getConfigurationSection("rewards")).getKeys(false)) {
 			rewards.put(s, config.getInt("rewards." + s));
 		}
+		ConfigurationSection messages = Objects.requireNonNull(config.getConfigurationSection("messages"));
+		prefix = (String) messages.get("prefix");
+		onlyPlayers = (String) messages.get("only-players");
+		noPermission = (String) messages.get("no-permission");
+		pluginInfo = (String) messages.get("plugin-info");
+		successfulReload = (String) messages.get("successful-reload");
+		help = (String) messages.get("help.normal");
+		helpReload = (String) messages.get("help.reload");
+		teleportHelp = (String) messages.get("teleport-help.normal");
+		teleportHelpVip = (String) messages.get("teleport-help.vip");
+		teleportHelpAdmin = (String) messages.get("teleport-help.admin");
 	}
 }
